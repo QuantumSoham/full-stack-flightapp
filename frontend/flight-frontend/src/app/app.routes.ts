179f1f18fb0e6bc98eee79877375f307055b5c78
QuantumSoham <quantumsc03@gmail.com>
2025-12-15 23:31:22 +0530

    added app routes

--- frontend/flight-frontend/src/app/app.routes.ts
+++ frontend/flight-frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './features/auth/login/login.component';
+import { RegisterComponent } from './features/auth/register/register.component';
+import { SearchFlightsComponent } from './features/flights/search-flights/search-flights.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'search-flights', component: SearchFlightsComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+];
