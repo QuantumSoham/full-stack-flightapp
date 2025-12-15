2029b9fc05c5205512b1154e0ffe725964c84d2a
QuantumSoham <quantumsc03@gmail.com>
2025-12-15 23:10:37 +0530

    added flight service

---
+++ frontend/flight-frontend/src/app/core/services/flight.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+// Marks this class as a service Angular can manage
+// providedIn: 'root' means there will be ONE shared instance
+// Any component that asks for FlightService gets the same one
+@Injectable({ providedIn: 'root' })
+export class FlightService {
+
+  // Base URL of the API Gateway
+  // All flight-related backend calls go through this
+  private baseUrl = 'http://localhost:8062';
+
+  // HttpClient is Angular’s built-in way to make HTTP requests
+  // Angular injects it automatically
+  constructor(private http: HttpClient) {}
+
+  // Sends a flight search request to the backend
+  // payload contains fromPlace, toPlace, date, seats, tripType, etc.
+  // Backend responds with a list of matching flights
+  searchFlights(payload: any): Observable<any> {
+    return this.http.post(
+      `${this.baseUrl}/api/v1.0/flight/search`,
+      payload
+    );
+  }
+}
