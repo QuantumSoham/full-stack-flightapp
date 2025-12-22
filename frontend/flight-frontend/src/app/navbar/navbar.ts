78ebc5b05bd7c29b549692d3eef1c44c92c68a01
QuantumSoham <quantumsc03@gmail.com>
2025-12-22 13:52:31 +0530

    conditionally render add flight button if user is admin on login

--- frontend/flight-frontend/src/app/navbar/navbar.ts
+++ frontend/flight-frontend/src/app/navbar/navbar.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../core/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -13,7 +14,7 @@ import { RouterLink } from '@angular/router';
 export class NavbarComponent {
   email = '';
 
-  constructor(private router: Router) {
+  constructor(private router: Router,public authservice: AuthService) {
     this.email = localStorage.getItem('userEmail') || '';
   }
 
